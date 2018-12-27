3288d2522049c14d398af75eecca0c0411cfec9e
Justin Michaud <justin@justinmichaud.com>
2018-12-27 11:44:01 -0500

    add bf jit functions

--- main.cc
+++ main.cc
@@ -11,7 +11,7 @@ public:
 	JITFnBlock()
 	{
 		block = (char*) mmap(NULL,       // address
-                      4096,             // size
+                      4096*24,             // size
                       PROT_READ | PROT_WRITE | PROT_EXEC,
                       MAP_PRIVATE | MAP_ANONYMOUS,
                       -1,               // fd (not used here)
@@ -22,12 +22,12 @@ public:
 
 	~JITFnBlock()
 	{
-		munmap((void*) block, 4096);
+		munmap((void*) block, 4096*24);
 		block = nullptr;
 	}
 
 	char* begin() const { return block; }
-	char* end() const { return block + 4096; }
+	char* end() const { return block + 4096*24; }
 };
 
 typedef struct REG_t {
@@ -39,6 +39,8 @@ const REG RDI = { 0b0111 };
 const REG RSI = { 0b0110 };
 const REG RDX = { 0b0010 };
 const REG RCX = { 0b0001 };
+const REG RBP = { 0b0101 };
+const REG RSP = { 0b0110 };
 
 extern "C" { typedef uint64_t (*jit_fp)(register void*, register uint64_t, register uint64_t, register uint64_t); }
 
@@ -46,8 +48,8 @@ class JITFn {
 	std::shared_ptr<JITFnBlock> block;
 	char* start;
 	char* end;
-public:
-	void write(std::initializer_list<unsigned char>&& data)
+
+	inline void write(std::initializer_list<unsigned char>&& data)
 	{
 		for (auto c : data) {
 			printf("%02x", (int) c);
@@ -57,6 +59,16 @@ public:
 		}
 	}
 
+	JITFn(std::shared_ptr<JITFnBlock> block, char* start)
+	{
+		this->block = std::move(block);
+		this->start = start;
+		end = start;
+	}
+
+	JITFn(const JITFn&) = delete;
+	JITFn(JITFn&&) = default;
+public:
 	JITFn()
 	{
 		block = std::make_shared<JITFnBlock>();
@@ -65,6 +77,7 @@ public:
 	}
 
 	void* current() const { return end; }
+	JITFn makeNextFunction() const { return JITFn(block, end); }
 
 	template <typename X>
 	uint64_t operator()(X* i)
@@ -86,6 +99,9 @@ public:
 	void jmp(REG r) { write({ 0xFF, (unsigned char) (0b11100000 | r.val) }); }
 	void call(REG r) { write({ 0xFF, (unsigned char) (0b11010000 | r.val) }); }
 
+	void push(REG r) { write({ (unsigned char) (0b01010000 | r.val) }); }
+	void pop(REG r) { write({ (unsigned char) (0b01011000 | r.val) }); }
+
 	void jump_if_equal(REG r, REG addr, uint8_t val) {
 		cmp(r, val);
 		jne(2);
@@ -104,44 +120,113 @@ public:
 			point[i] = (char) ((asInt>>(i*8))&0x00FF);
 	}
 
+	void prologue(const std::string& name) {
+		std::cerr << "Emitting " << name << ": \n";
+        	push(RBP);
+        	mov(RBP, RSP);
+	}
+
+	void epilogue() {
+		pop(RBP);
+        	ret();
+        	std::cerr << "\n*****\n";
+	}
 };
 
-uint64_t bf_print(char* data) {
-	std::cout << "BF output:\n";
+extern "C" uint64_t bf_print(register char* data) {
 	std::cout << *data;
-	std::cout << "BF output done\n";
-	return 5;
+	return 0;
+}
+
+extern "C" uint64_t bf_input(register char* data) {
+        std::cin >> *data;
+        return 0;
 }
 
 // Calling convention: RAX - return
 // Input/scratch: RDI, RSI, RDX, RCX, ... (2 more) then stack
-void ident(JITFn& f)
+
+
+// These helpers assume BF data is in RDI
+void bf_jit_output(JITFn& f)
 {
-	std::cerr << "Emitting ident: \n";
+	f.push(RDI);
+	f.load(RAX, (uint64_t) ((void*) bf_print));
+	f.call(RAX);
+	f.pop(RDI);
+}
+
+void bf_jit_input(JITFn& f)
+{
+        f.push(RDI);
+        f.load(RAX, (uint64_t) ((void*) bf_input));
+        f.call(RAX);
+        f.pop(RDI);
+}
+
+void bf_jit_inc_data(JITFn& f)
+{
 	f.load(RAX, RDI, 0);
-	f.load(RSI, 2);
+	f.load(RSI, 1);
 	f.add(RAX, RSI);
 	f.store(RDI, 0, RAX);
+}
 
-	// TODO save / restore registers
-	f.load(RSI, (uint64_t) ((void*) bf_print));
-	f.call(RSI);
-	
-	f.ret();
-	std::cerr << "\n*****\n";
+void bf_jit_inc_data_ptr(JITFn& f)
+{
+        f.load(RSI, 1);
+        f.add(RDI, RSI);
+}
+
+void bf_jit_dec_data(JITFn& f)
+{
+        f.load(RAX, RDI, 0);
+        f.load(RSI, 1);
+        f.sub(RAX, RSI);
+        f.store(RDI, 0, RAX);
+}
+
+void bf_jit_sub_data_ptr(JITFn& f)
+{
+        f.load(RSI, 1);
+        f.sub(RDI, RSI);
+}
+
+char* bf_jit_jump(JITFn& f, void* to)
+{
+	char* patch = f.make_patchpoint(RSI);
+	f.patch(patch, (void*) to);
+	f.jmp(RSI);
+	return patch;
+}
+
+char* bf_jit_jump_if_zero(JITFn& f, void* to)
+{
+	char* patch = f.make_patchpoint(RSI);
+	f.patch(patch, (void*) to);
+	f.load(RDX, RDI, 0);
+	f.jump_if_equal(RDX, RSI, 0);
+	return patch;
 }
 
 int main()
 {
 	JITFn fn;
-	ident(fn);
+	fn.prologue("jit_entry");
+	auto* begin_loop = fn.current();
 
-	uint64_t val = 42;
+	bf_jit_input(fn);
+	for (int i=0; i<'h'; ++i) bf_jit_dec_data(fn);
+	auto* end_loop_patch = bf_jit_jump_if_zero(fn, (void*)nullptr);
+	bf_jit_jump(fn, begin_loop);
 
-	std::cout << fn(&val) << "\n";
-	std::cout << val << "\n";
+	fn.patch(end_loop_patch, fn.current());
+	for (int i=0; i<'h'; ++i) bf_jit_inc_data(fn);
+	bf_jit_output(fn);
+	fn.epilogue();
 
-	//for (int i=0; i<10; ++i)
-	//	std::cout << "f(" << i << ") = " << fn(i) << ";\n";
+	uint64_t val = 0;
+	fn(&val);
+
 	return 0;
 }
